cd1bf287f106c855183c5c7569af9f0f0a71ee7a
cavassini <thiagocavassinipc@gmail.com>
2022-10-27 11:23:04 -0300

    adicionando arquivo oracle.sh e comex.jar

---
+++ oracle.sh
@@ -0,0 +1,4 @@
+docker run -d -p 1521:1521 -e ORACLE_PASSWORD=cavacini gvenzl/oracle-xe
+
+precisei usar essa senha que eu configurei quando instalei o oracle, pois nao estava dando certo com a senha admin
+
